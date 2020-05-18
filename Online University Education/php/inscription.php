5f43e49cf72b79d31ec81ec2995f75b7920c6319
badr-aydi <badr.aydi@isimsf.u-sfax.tn>
2020-05-18 23:01:17 +0100

    int.

--- Online University Education/php/inscription.php
+++ Online University Education/php/inscription.php
@@ -1,5 +1,9 @@
 <?php
 
+session_start();
+header('location:inscription.php');
+
+
 $co = mysqli_connect("localhost", "root", "") or die("erreur");
 mysqli_select_db($co,"elue") or die("erreur de selection de BD");
 
@@ -16,7 +20,7 @@ $res1=mysqli_query($co,$id) or die("requete erone");
 $enreg=mysqli_fetch_array($res1);
 $n=$enreg[0]+1;
 
-if($password1==$password2){
+if($password1!=$password2){
     $INSERT_U = "INSERT INTO utilisateur values ('$n','e','$nom','$prenom','$email','$password1')"; 
     
     $res = mysqli_query($co,$INSERT_U)or die ("verifier ta requete INSERT_U".mysqli_error());
